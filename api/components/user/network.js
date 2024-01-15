1b0bb0bbf31ebaa68a93e8be95dfd07af0fe8232
Irvin120 <iirvin.cueto.02@gmail.com>
2024-01-15 17:00:13 -0600

    Iniciando

---
+++ api/components/user/network.js
@@ -0,0 +1,14 @@
+//api/components/user/network.js
+
+const express = require('express')
+const response = require('../../../network/response')
+
+const  router = express.Router();
+
+router.get ('/', function (req, res){
+    response.success(req, res, 'Todo correcto', 200)
+})
+
+
+
+module.exports = router; 
