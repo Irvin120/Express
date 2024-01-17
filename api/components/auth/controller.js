e7217d05d00235ef0c5cd1e11fe62738a3e56e8c
Irvin120 <iirvin.cueto.02@gmail.com>
2024-01-16 22:21:08 -0600

    bcryp

--- api/components/auth/controller.js
+++ api/components/auth/controller.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const auth = require('../../../auth')
 const TABLA = 'auth';
 
@@ -9,14 +10,19 @@ module.exports = function(injectedStore){
 
     async function login(username, password){
         const data = await store.query(TABLA, { username: username})
-        if(data.password === password){
-            return auth.sign(data)
-        }else{
-            throw new Error('Informacion invalida')
-        }
+
+        return bcrypt.compare(password, data.password)
+        .then(sonIguales =>{
+            if(sonIguales === true){
+                return auth.sign(data)
+            }else{
+                throw new Error('Informacion invalida')
+            }
+        })
+     
     }
 
-    function upsert(data){
+    async function upsert(data){
         const authData = {
             id: data.id,
         }       
@@ -26,7 +32,7 @@ module.exports = function(injectedStore){
         }
 
         if(data.password){
-            authData.password = data.password;
+            authData.password = await bcrypt.hash(data.password, 5);
         }
 
         return store.upsert(TABLA, authData);
